adc3dd77b0ec9c47f9563f35b3d3ab3e9a792fc4
HimikaP <prabhat.himika@gmail.com>
2023-12-03 22:22:40 +0530

    a commit

---
+++ Week 1/class1.js
@@ -0,0 +1,27 @@
+console.log("it is working")
+
+const personArray = ["himika", "ikki", "jyots"]
+const gender = ["nonbinary", "alien", "girl"]
+
+// const user1 = [{
+//     firstName: "himika",
+//     gender: "male"
+// }, {
+//     firstName: "ikki",
+//     gender: "alien"
+// }, {
+//     firstName: "jyots",
+//     gender: "fimale"
+// }
+// ]
+
+// for(let i=0;i< user1.length;i++){
+//     if(user1[i]["gender"] == 'alien')
+//     console.log("Ikki")
+// }
+
+
+function sum(a,b){
+    return a+b;
+}
+
